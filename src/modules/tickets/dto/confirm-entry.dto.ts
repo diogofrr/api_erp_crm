8293262d2dbf77527656758b234405a0850693bd
Diogo Henrique Ferreira <ddiogof20@gmail.com>
2025-07-20 12:08:05 -0300

    fix(tickets): add qrCode to confirmEntry

--- src/modules/tickets/dto/confirm-entry.dto.ts
+++ src/modules/tickets/dto/confirm-entry.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class ConfirmEntryDto {
   @IsNotEmpty({ message: 'eventId não pode ser vazio' })
@@ -8,4 +8,8 @@ export class ConfirmEntryDto {
   @IsNotEmpty({ message: 'ticketId não pode ser vazio' })
   @IsUUID(undefined, { message: 'ticketId deve ser um UUID válido' })
   ticketId: string;
+
+  @IsNotEmpty({ message: 'qrCode não pode ser vazio' })
+  @IsString({ message: 'qrCode deve ser uma string' })
+  qrCode: string;
 }
